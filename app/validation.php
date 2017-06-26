a6b48f58767f539ff84a0d83ae5f7eed8bd85fc1
Pat Esty <patrickesty@hotmail.com>
2017-06-26 00:11:04 -0400

    added use of validator class 2

--- app/validation.php
+++ app/validation.php
@@ -12,12 +12,28 @@ function OutputValidatorObject() {
 }
 
 function GetValidDateString($date_raw) {
-  //use the Validator class to validate valid input:
-  if (Validator::notEmpty()->validate($date_raw)) {
-    //Format returns format: Jan 1st 2016, etc.
-    return date('F jS Y', $date_raw);
+  //$date_validator = Validator::notEmpty()->date();
+  try {
+    //use the Validator class to validate valid input:
+    if (Validator::notEmpty()->validate($date_raw)) {
+    //if ($date_validator->validate($date_raw)) {
+      //Format returns format: Jan 1st 2016, etc.
+      return date('F jS Y', $date_raw);
+    };
+
+    //NOTE: can also do this:
+    //$date_validator = Validator::notEmpty()->date();
+    //if ($date_validator->validate($date_raw)) {...}
+
+  //If using ->assert, use NestledValidationException
+  } catch (Exception $e) {
+    return $e->getMessages();
   };
-  //NOTE: can also do this:
-  //$date_validator = Validator::date()->notEmpty();
-  //if ($date_validator->validate($date_raw)) {...}
+}
+
+function GetValidEMail($email_test) {
+  $email_validator = Validator::email()->notEmpty();
+  if ($email_validator->validate($email_test)) {
+    return $email_test;
+  }
 }
